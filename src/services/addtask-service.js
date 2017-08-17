474c4e221277bc271378e8291803b60074b02ddc
mizueone <orlinskikyle@gmail.com>
2017-08-16 23:15:36 -0500

    added addtask controller, addtask-service and finished the interface for adding a task

---
+++ src/services/addtask-service.js
@@ -0,0 +1,9 @@
+(function() {
+  angular.module('pomodoro').service('addtask-service', function() {
+    return {
+      addTask: function(taskName, taskDescription, timeEstimate) {
+
+      }
+    }
+  });
+})();
