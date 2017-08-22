4e573f6955f838942ca99e9c9ff6a4cbb9b1ae8b
mizueone <orlinskikyle@gmail.com>
2017-08-21 22:13:08 -0500

    Creating Angular Directive for each task

--- src/controllers/addtask-controller.js
+++ src/controllers/addtask-controller.js
@@ -1,7 +1,12 @@
 (function() {
   angular.module('pomodoro').controller('addtask', ['$scope', function($scope) {
+    //each day will have a different color
     $scope.taskName = '';
     $scope.taskDescription = '';
     $scope.timeEstimate = '';
-  }]);
+  }]).directive('task', function() {
+    return {
+      templateUrl: './src/directives/task-directive.html'
+    }
+  });
 })();
